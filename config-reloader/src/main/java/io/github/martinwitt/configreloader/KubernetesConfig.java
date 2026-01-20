b356f87dd2df60ac1c4bbf4aa3f5ae3a3f6b9a0d
Martin Wittlinger <wittlinger.martin@gmail.com>
2026-01-20 21:30:12 +0100

    fix native image jackson

--- config-reloader/src/main/java/io/github/martinwitt/configreloader/KubernetesConfig.java
+++ config-reloader/src/main/java/io/github/martinwitt/configreloader/KubernetesConfig.java
@@ -2,11 +2,9 @@ package io.github.martinwitt.configreloader;
 
 import io.fabric8.kubernetes.client.KubernetesClient;
 import io.fabric8.kubernetes.client.KubernetesClientBuilder;
-import org.springframework.aot.hint.annotation.RegisterReflectionForBinding;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-@RegisterReflectionForBinding({io.fabric8.kubernetes.client.impl.KubernetesClientImpl.class})
 @Configuration
 public class KubernetesConfig {
 
