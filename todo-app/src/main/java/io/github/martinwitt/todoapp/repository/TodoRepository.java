8870e1f0134ef44834ee35ea68e9bf7942cdbd8a
Martin Wittlinger <wittlinger.martin@gmail.com>
2026-04-05 17:44:54 +0200

    spotless

--- todo-app/src/main/java/io/github/martinwitt/todoapp/repository/TodoRepository.java
+++ todo-app/src/main/java/io/github/martinwitt/todoapp/repository/TodoRepository.java
@@ -6,5 +6,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface TodoRepository extends JpaRepository<Todo, Long> {
     List<Todo> findAllByOrderByPositionAscDeadlineAsc();
+
     List<Todo> findByTags_NameOrderByPositionAscDeadlineAsc(String name);
 }
