e3a1202304979af179bb5d666111417db953de10
Martin Wittlinger <wittlinger.martin@gmail.com>
2026-05-12 10:02:55 +0200

    refactor: add List import for improved type handling in HelmVersionCheckService

--- image-update-detector/src/main/java/io/github/martinwitt/imagedetector/service/HelmVersionCheckService.java
+++ image-update-detector/src/main/java/io/github/martinwitt/imagedetector/service/HelmVersionCheckService.java
@@ -6,6 +6,7 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
