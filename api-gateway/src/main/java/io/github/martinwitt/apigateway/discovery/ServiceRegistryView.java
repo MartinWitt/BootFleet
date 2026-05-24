99d9f3ee75c65b3fa50258d95098fa34de211bbe
Martin Wittlinger <wittlinger.martin@gmail.com>
2026-05-24 15:15:25 +0200

    cleanup

--- api-gateway/src/main/java/io/github/martinwitt/apigateway/discovery/ServiceRegistryView.java
+++
@@ -1,7 +0,0 @@
-package io.github.martinwitt.apigateway.discovery;
-
-import java.time.Instant;
-import java.util.List;
-
-public record ServiceRegistryView(
-        List<RouteInfo> routes, List<String> discoveredServices, Instant timestamp) {}
