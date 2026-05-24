93c1b5b2afeb61bb9632b7ba68ffd7a22a6dabef
Martin Wittlinger <wittlinger.martin@gmail.com>
2026-05-25 01:26:40 +0200

    feat: add Liquibase native hints and update reachability metadata for reflection

--- todo-app/src/main/java/io/github/martinwitt/todoapp/TodoAppApplication.java
+++ todo-app/src/main/java/io/github/martinwitt/todoapp/TodoAppApplication.java
@@ -2,8 +2,10 @@ package io.github.martinwitt.todoapp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ImportRuntimeHints;
 
 @SpringBootApplication
+@ImportRuntimeHints(LiquibaseNativeHints.class)
 public class TodoAppApplication {
     public static void main(String[] args) {
         SpringApplication.run(TodoAppApplication.class, args);
