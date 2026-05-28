8b06b239b0e913301237ef9e220db2e8ce32040d
Martin Wittlinger <wittlinger.martin@gmail.com>
2026-05-28 20:22:29 +0200

    feat: overhaul UI to Material Design (#81)

--- todo-app/src/main/java/io/github/martinwitt/todoapp/domain/TodoStatus.java
+++ todo-app/src/main/java/io/github/martinwitt/todoapp/domain/TodoStatus.java
@@ -3,5 +3,17 @@ package io.github.martinwitt.todoapp.domain;
 public enum TodoStatus {
     OPEN,
     IN_PROGRESS,
-    DONE
+    DONE;
+
+    @Override
+    public String toString() {
+        String[] words = name().split("_");
+        StringBuilder sb = new StringBuilder();
+        for (String word : words) {
+            if (!sb.isEmpty()) sb.append(' ');
+            sb.append(Character.toUpperCase(word.charAt(0)));
+            sb.append(word.substring(1).toLowerCase());
+        }
+        return sb.toString();
+    }
 }
