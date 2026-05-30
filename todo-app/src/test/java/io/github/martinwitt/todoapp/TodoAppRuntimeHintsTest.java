773a4f42bede730cf69680f4728002ac01d5ad46
Martin Wittlinger <wittlinger.martin@gmail.com>
2026-05-30 15:42:29 +0200

    test: add runtime hints tests for domain types and NotBlankValidator

---
+++ todo-app/src/test/java/io/github/martinwitt/todoapp/TodoAppRuntimeHintsTest.java
@@ -0,0 +1,31 @@
+package io.github.martinwitt.todoapp;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
+import io.github.martinwitt.todoapp.domain.Tag;
+import io.github.martinwitt.todoapp.domain.Todo;
+import io.github.martinwitt.todoapp.domain.TodoStatus;
+import org.hibernate.validator.internal.constraintvalidators.bv.NotBlankValidator;
+import org.junit.jupiter.api.Test;
+import org.springframework.aot.hint.RuntimeHints;
+import org.springframework.aot.hint.predicate.RuntimeHintsPredicates;
+
+class TodoAppRuntimeHintsTest {
+
+    private final RuntimeHints hints = new RuntimeHints();
+
+    @Test
+    void domainTypesAreRegistered() {
+        new TodoAppRuntimeHints().registerHints(hints, Thread.currentThread().getContextClassLoader());
+        assertThat(RuntimeHintsPredicates.reflection().onType(Todo.class).test(hints)).isTrue();
+        assertThat(RuntimeHintsPredicates.reflection().onType(Tag.class).test(hints)).isTrue();
+        assertThat(RuntimeHintsPredicates.reflection().onType(TodoStatus.class).test(hints)).isTrue();
+    }
+
+    @Test
+    void notBlankValidatorIsRegisteredForReflection() {
+        new TodoAppRuntimeHints().registerHints(hints, Thread.currentThread().getContextClassLoader());
+        assertThat(RuntimeHintsPredicates.reflection().onType(NotBlankValidator.class).test(hints))
+                .isTrue();
+    }
+}
