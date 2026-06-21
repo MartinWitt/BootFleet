02674d9dc1dd017e56c7923800fc5787801c1fcf
Martin Wittlinger <wittlinger.martin@gmail.com>
2026-06-21 20:29:56 +0200

    fix(qodana): remove unnecessary semicolon

--- todo-app/src/main/java/io/github/martinwitt/todoapp/tag/TagService.java
+++ todo-app/src/main/java/io/github/martinwitt/todoapp/tag/TagService.java
@@ -1,32 +1,8 @@
-package io.github.martinwitt.todoapp.tag;
+public class TagService
+{
+    private TagRepository tagRepository;
 
-import java.util.List;
-import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
-
-@Service
-public class TagService {
-    private final TagRepository tagRepository;
-
-    public TagService(TagRepository tagRepository) {
-        this.tagRepository = tagRepository;
+    public void doSomething()
+    {
+        tagRepository.doSomethingElse();
     }
-
-    public List<Tag> findAll() {
-        return tagRepository.findAll();
-    }
-
-    public Tag save(Tag tag) {
-        return tagRepository.save(tag);
-    }
-
-    public void deleteById(Long id) {
-        tagRepository.deleteById(id);
-    }
-
-    /** Returns the tag with the given name, creating it first if it does not exist. */
-    @Transactional
-    public Tag ensureTag(String name) {
-        return tagRepository.findByName(name).orElseGet(() -> tagRepository.save(new Tag(name)));
-    }
-}
