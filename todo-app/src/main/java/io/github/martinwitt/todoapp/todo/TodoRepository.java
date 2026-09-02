54346884525da39d758969ca9ec9dc73d494c6ad
Martin Wittlinger <wittlinger.martin@gmail.com>
2026-09-02 15:15:57 +0200

    fix: ClassEscapesItsScope in todo-app/src/main/java/io/github/martinwitt/todoapp/todo/TodoService.java (gemma4:e4b) (#234)
    Co-authored-by: codesweeper[bot] <codesweeper[bot]@users.noreply.github.com>

--- todo-app/src/main/java/io/github/martinwitt/todoapp/todo/TodoRepository.java
+++ todo-app/src/main/java/io/github/martinwitt/todoapp/todo/TodoRepository.java
@@ -4,7 +4,7 @@ import java.time.LocalDateTime;
 import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-interface TodoRepository extends JpaRepository<Todo, Long> {
+public interface TodoRepository extends JpaRepository<Todo, Long> {
     List<Todo> findAllByOrderByPositionAscDeadlineAsc();
 
     List<Todo> findByTags_NameOrderByPositionAscDeadlineAsc(String name);
